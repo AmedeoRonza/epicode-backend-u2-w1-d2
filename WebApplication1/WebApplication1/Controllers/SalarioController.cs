77125b9abfcaa57c3ace8aa2150f923ddbce28f8
Amedeo Ronza <amedeoronza@gmail.com>
2024-02-27 17:06:59 +0100

    ParteUno

---
+++ WebApplication1/WebApplication1/Controllers/SalarioController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SalarioController : Controller
+    {
+        // GET: Salario
+        public ActionResult Index()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionStringDB"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Pagamenti", conn);
+            List<Salario> paymentList = new List<Salario>();
+            try
+            {
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Salario soldi = new Salario(
+                        Convert.ToInt32(reader["IdPagamenti"]),
+                        reader["Nome"].ToString(),
+                        reader["Cognome"].ToString(),
+                        reader["DataPagamento"].ToString(),
+                        Convert.ToInt32(reader["Pagamento"]),
+                        Convert.ToBoolean(reader["Stipendio"]),
+                        Convert.ToBoolean(reader["Acconto"]));
+
+                    paymentList.Add(soldi);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Errore");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close(); // Chiudo la connessione al db, NECESSARIO
+            }
+            return View(paymentList);            
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Salario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Salario
+    {
+        public int IdPagamenti { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string DataPagamento { get; set; }
+        public int Pagamento { get; set; }
+        public bool Stipendio {  get; set; }
+        public bool Acconto { get; set; }
+
+        public Salario() { }
+        public Salario(int idPagamenti, string nome, string cognome, string dataPagamento, int pagamento, bool stipendio, bool acconto)
+        {
+            IdPagamenti = idPagamenti;
+            Nome = nome;
+            Cognome = cognome;
+            DataPagamento = dataPagamento;
+            Pagamento = pagamento;
+            Stipendio = stipendio;
+            Acconto = acconto;
+        }
+
+    }
+}
